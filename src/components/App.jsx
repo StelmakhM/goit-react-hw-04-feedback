df58dadd497c79a7a32d71e1c7792c393b4a336e
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-12-30 18:54:18 +0200

    mentor corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,38 +2,65 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [reviews, setReviews] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [totalReviews, setTotalReviews] = useState({
+    good,
+    neutral,
+    bad,
+  });
+
+  useEffect(() => {
+    setTotalReviews({
+      good,
+      neutral,
+      bad,
+    });
+  }, [good, neutral, bad]);
 
   const onLeaveFeedback = option => {
-    setReviews(prevReviews => ({
-      ...prevReviews,
-      [option]: prevReviews[option] + 1,
-    }));
+    switch (option[0]) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        throw new Error('Unexpected input error');
+    }
   };
 
   const countTotalFeedback = () => {
-    return Object.values(reviews).reduce((acc, current) => acc + current);
+    return Object.values(totalReviews).reduce((acc, current) => acc + current);
   };
 
   const countPositiveFeedbackPercentage = () => {
-    const percentage = Math.floor((reviews.good / countTotalFeedback()) * 100);
+    const percentage = Math.floor((good / countTotalFeedback()) * 100);
     return isFinite(percentage) ? `${percentage} %` : '';
   };
 
   return (
     <>
       <Section title="Please, leave feedback">
-        <FeedbackOptions onLeaveFeedback={onLeaveFeedback} options={reviews} />
+        <FeedbackOptions
+          onLeaveFeedback={onLeaveFeedback}
+          options={totalReviews}
+        />
       </Section>
       {countTotalFeedback() ? (
         <Section title="Statistics">
           <Statistics
-            good={reviews.good}
-            neutral={reviews.neutral}
-            bad={reviews.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
